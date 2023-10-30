e7b01ee07922e0337009356830697a047bc83aee
yct12321 <yct37785@gmail.com>
2023-10-30 20:40:13 +0800

    [refactored] streamlined promise hell for getLocalUserData and auxillary functions

--- Framework/Managers/LocalDataManager.js
+++ Framework/Managers/LocalDataManager.js
@@ -8,19 +8,16 @@ const _ = require('lodash');
  * and a deep copy returned
  *------------------------------------------------------------------------------------*/
 async function createNewUserData(NEW_USER_DATA) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // flatten to key value pairs to save individually as dictated by api
-      const keyValList = Object.keys(NEW_USER_DATA).map((key) => {
-        return [key, JSON.stringify(NEW_USER_DATA[key])];
-      });
-      await WriteDataAS(keyValList);
-      resolve(_.cloneDeep(NEW_USER_DATA));
-    } catch (e) {
-      console.log(e);
-      reject(e);
-    }
-  });
+  try {
+    const keyValList = Object.keys(NEW_USER_DATA).map((key) => {
+      return [key, JSON.stringify(NEW_USER_DATA[key])];
+    });
+    await writeDataAS(keyValList);
+    return _.cloneDeep(NEW_USER_DATA);
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 }
 
 /**------------------------------------------------------------------------------------*
@@ -30,20 +27,13 @@ async function createNewUserData(NEW_USER_DATA) {
  * handled separately (i.e. migration patches)
  *------------------------------------------------------------------------------------*/
 function fixNestedKeyValues(currObj, templateObj) {
-  if (typeof currObj !== 'object') {
-    return false;
-  }
   let hasMissing = false;
   for (const key in templateObj) {
-    // if key not in curr obj
     if (!(key in currObj)) {
       currObj[key] = _.cloneDeep(templateObj[key]);
       hasMissing = true;
-    } else {
-      // check nested
-      if (checkNested(currObj[key], templateObj[key])) {
-        hasMissing = true;
-      }
+    } else if (fixNestedKeyValues(currObj[key], templateObj[key])) {
+      hasMissing = true;
     }
   }
   return hasMissing;
@@ -54,36 +44,30 @@ function fixNestedKeyValues(currObj, templateObj) {
  * key value pair is missing
  *------------------------------------------------------------------------------------*/
 async function getLocalUserData(NEW_USER_DATA) {
-  return new Promise(async (resolve, reject) => {
-    try {
-      // get all rows
-      let allKeys = await getAllKeysAS();
-      // DEBUG ONLY, REMOVE
-      if (allKeys.length > 0) {
-        await deleteDataAS(allKeys);
-        allKeys = [];
-      }
-      // read data
-      let userData = {};
-      // new user, create new user data
-      if (allKeys.length === 0) {
-        userData = await createNewUserData(NEW_USER_DATA);
-      } else {
-        // read all keys, even if not part of NEW_USER_DATA
-        userData = await readDataAS(allKeys);
-        // check if keys (root/nested) missing, fill in with default values from NEW_USER_DATA
-        const hasMissing = fixNestedKeyValues(userData, NEW_USER_DATA);
-        if (hasMissing) {
-          await WriteDataAS(objToKeyValueArr(userData));
-          console.log('saved userData for missing keys');
-        }
-      }
-      resolve(userData);
-    } catch(e) {
-      console.log(e);
-      reject(e);
+  try {
+    let allKeys = await getAllKeysAS();
+    
+    // DEBUG ONLY, REMOVE
+    if (allKeys.length > 0) {
+      await deleteDataAS(allKeys);
+      allKeys = [];
     }
-  });
+
+    if (allKeys.length === 0) {
+      return await createNewUserData(NEW_USER_DATA);
+    } else {
+      const userData = await readDataAS(allKeys);
+      const hasMissing = fixNestedKeyValues(userData, NEW_USER_DATA);
+      if (hasMissing) {
+        await writeDataAS(objToKeyValueArr(userData));
+        console.log('saved userData for missing keys');
+      }
+      return userData;
+    }
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
 }
 
 /**------------------------------------------------------------------------------------*
