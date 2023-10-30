aa6e3d7334e23d987f94e78c3164d58a00e61760
Yct <yct37785@gmail.com>
2023-10-30 15:44:42 +0800

    [refactor] rename DefaultValues to Schemas and move to User folder

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // change to import RootComp from '../<expo-sample-app-folder>/Framework/RootComp';
 import RootComp from './Framework/RootComp';
 import { screenMaps, DEFAULT_SCREEN } from './User/PageMapper';
-import { NEW_USER_DATA, APP_NAME } from './DefaultValues';
+import { NEW_USER_DATA, APP_NAME } from './User/Schemas';
 
 const App = () => {
   return (
