ed24db99a9f53db387731e47656092a4a04d7894
yct12321 <yct37785@gmail.com>
2024-05-29 03:09:30 +0800

    [refactor] localDataContext will not be exposed to end users, hooks to expose functions instead

--- Framework/Contexts/LocalDataContext.js
+++ Framework/Contexts/LocalDataContext.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React, { createContext, useContext, useEffect } from 'react';
 
 /**
- * For any user data
+ * will not be exposed to end users
  */
 export const LocalDataContext = React.createContext({
   updateCount: 0,
@@ -10,3 +10,35 @@ export const LocalDataContext = React.createContext({
   resetLocalData: () => {},
   getLocalDataStringify: () => {},
 });
+
+/**
+ * callback when local data updates
+ */
+export const onLocalDataUpdate = (callback) => {
+  const { updateCount } = useContext(LocalDataContext);
+
+  useEffect(() => {
+    if (updateCount) {
+      callback();
+    }
+  }, [updateCount, callback]);
+};
+
+/**
+ * expose context functions/values
+ */
+export const setLocalDataValue = () => {
+  return { setLocalDataValue } = useContext(LocalDataContext);
+};
+
+export const getLocalDataValue = () => {
+  return { getLocalDataValue } = useContext(LocalDataContext);
+};
+
+export const resetLocalData = () => {
+  return { resetLocalData } = useContext(LocalDataContext);
+};
+
+export const getLocalDataStringify = () => {
+  return { getLocalDataStringify } = useContext(LocalDataContext);
+};
