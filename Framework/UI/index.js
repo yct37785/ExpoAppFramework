10dac975b9b1bc6fa03bfa6867026f9fed537f68
yct12321 <yct37785@gmail.com>
2024-05-30 23:12:57 +0800

    [feat] expose UI comps via index file to better organize imports

---
+++ Framework/UI/index.js
@@ -0,0 +1,19 @@
+/**
+ * exposed to end users, export new components here
+ */
+// Collapsible.js
+export { default as CollapsibleComp } from './Collapsible';
+export { default as AccordionComp } from './Collapsible';
+// Dialog.js
+export { default as Dialog } from './Dialog';
+// DropdownCheckMenu.js
+export { default as DropdownCheckMenu } from './DropdownCheckMenu';
+// DropdownMenu.js
+export { default as DropdownMenu } from './DropdownMenu';
+// Options.js
+export { default as ChipsComp } from './Options';
+// Picker.js
+export { default as PickerComp } from './Picker';
+// search bar todo
+// TabBar.js
+export { default as TabBarComp } from './TabBar';
