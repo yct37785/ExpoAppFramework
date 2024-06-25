ffcfb45b060caeebc93dd24de9ce17ab264ab136
yct12321 <yct37785@gmail.com>
2024-06-25 23:52:50 +0800

    [refactor] revamp SampleLayoutsPage

--- projects/TemplateApp/Pages/SampleLayoutsPage.js
+++ projects/TemplateApp/Pages/SampleLayoutsPage.js
@@ -18,8 +18,8 @@ import { padSize } from '../../../Framework/Common/Values';
 export default function SampleLayoutsPage({ navigation, route, screenHeaderComp: ScreenHeaderComp }) {
   const theme = useTheme();
 
-  const TextContainer = ({ color, text, style, ...props }) => (
-    <View style={[{ backgroundColor: color }, style]}>
+  const TextContainer = ({ text, style, ...props }) => (
+    <View style={[{ backgroundColor: '#be74c8' }, style]}>
       <Text>{text}</Text>
     </View>
   );
@@ -35,43 +35,51 @@ export default function SampleLayoutsPage({ navigation, route, screenHeaderComp:
       <LinearLayout childMargin={padSize} align='vertical' scrollable={true} style={{ padding: padSize }}>
         {/* horizontal layout: child = wrap content */}
         <Text>horizontal layout: child = wrap content</Text>
-        <LinearLayout childMargin={padSize} flex={0} align='horizontal' style={{ backgroundColor: '#991f00', padding: padSize }}>
-          <TextContainer color="blue" text="Horizontal Item 1" />
-          <TextContainer color="blue" text="Horizontal Item 2" />
+        <LinearLayout childMargin={padSize} flex={0} align='horizontal' style={{ backgroundColor: '#78dd8d', padding: padSize }}>
+          <TextContainer text="Horizontal Item 1" />
+          <TextContainer text="Horizontal Item 2" />
         </LinearLayout>
         {/* horizontal layout: child = match parent */}
         <Text>horizontal layout: child = match parent</Text>
-        <LinearLayout childMargin={padSize} flex={0} align='horizontal' childLayout = 'match-parent' style={{ backgroundColor: '#991f00', padding: padSize }}>
-          <TextContainer color="blue" text="Horizontal Item 1" />
-          <TextContainer color="blue" text="Horizontal Item 2" />
+        <LinearLayout childMargin={padSize} flex={0} align='horizontal' childLayout = 'match-parent' style={{ backgroundColor: '#78dd8d', padding: padSize }}>
+          <TextContainer text="Horizontal Item 1" />
+          <TextContainer text="Horizontal Item 2" />
         </LinearLayout>
         {/* grid layout: child = wrap content */}
         <Text>grid layout: child = wrap content</Text>
-        <GridLayout flex={0} childMargin={padSize} columns={4} childLayout = 'wrap-content' style={{ backgroundColor: 'lightblue', padding: padSize }}>
-          <TextContainer color="blue" text="Grid Item 1" />
-          <TextContainer color="blue" text="Grid Item 2" />
-          <TextContainer color="blue" text="Grid Item 3" />
-          <TextContainer color="blue" text="Grid Item 4" />
-          <TextContainer color="blue" text="Grid Item 5" />
-          <TextContainer color="blue" text="Grid Item 6" />
+        <GridLayout flex={0} childMargin={padSize} columns={4} childLayout = 'wrap-content' style={{ backgroundColor: '#78c9dd', padding: padSize }}>
+          <TextContainer text="Grid Item 1" />
+          <TextContainer text="Grid Item 2" />
+          <TextContainer text="Grid Item 3" />
+          <TextContainer text="Grid Item 4" />
+          <TextContainer text="Grid Item 5" />
+          <TextContainer text="Grid Item 6" />
         </GridLayout>
         {/* grid layout: child = match parent */}
         <Text>grid layout: child = match parent</Text>
-        <GridLayout flex={0} childMargin={padSize} columns={4} childLayout = 'match-parent' style={{ backgroundColor: 'lightblue', padding: padSize }}>
-          <TextContainer color="blue" text="Grid Item 1" />
-          <TextContainer color="blue" text="Grid Item 2" />
-          <TextContainer color="blue" text="Grid Item 3" />
-          <TextContainer color="blue" text="Grid Item 4" />
-          <TextContainer color="blue" text="Grid Item 5" />
-          <TextContainer color="blue" text="Grid Item 6" />
+        <GridLayout flex={0} childMargin={padSize} columns={4} childLayout = 'match-parent' style={{ backgroundColor: '#78c9dd', padding: padSize }}>
+          <TextContainer text="Grid Item 1" />
+          <TextContainer text="Grid Item 2" />
+          <TextContainer text="Grid Item 3" />
+          <TextContainer text="Grid Item 4" />
+          <TextContainer text="Grid Item 5" />
+          <TextContainer text="Grid Item 6" />
         </GridLayout>
         {/* vertical layout: child = wrap content */}
         <Text>vertical layout: child = wrap content</Text>
-        <LinearLayout childMargin={padSize} flex={0} align='vertical' style={{ backgroundColor: '#991f00', padding: padSize }}>
-          <TextContainer color="blue" text="Horizontal Item 1" />
-          <TextContainer color="blue" text="Horizontal Item 2" />
+        <LinearLayout childMargin={padSize} flex={0} align='vertical' style={{ backgroundColor: '#d7dd78', padding: padSize }}>
+          <TextContainer text="Horizontal Item 1" />
+          <TextContainer text="Horizontal Item 2" />
         </LinearLayout>
-        <View style={{ height: 1500, backgroundColor: 'green', marginTop: padSize }}></View>
+        <View style={{ height: 1500, backgroundColor: '#ce7650', marginTop: padSize, padding: padSize }}>
+          <Text variant="bodyLarge">scroll down...</Text>
+          <Text variant="bodyMedium">down...</Text>
+          <Text variant="bodySmall">down...</Text>
+          <Text style={{ fontSize: 10 }}>down...</Text>
+          <Text style={{ fontSize: 8 }}>down...</Text>
+          <Text style={{ fontSize: 6 }}>down...</Text>
+          <Text style={{ fontSize: 4 }}>down...</Text>
+        </View>
       </LinearLayout>
     </LinearLayout>
   );
