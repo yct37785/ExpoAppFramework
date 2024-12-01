665444104c1bbb76c8de4270f059ac6d3e93f73d
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2024-12-01 21:40:04 +0800

    🔄 wildcard export for general utils functions

--- Framework/Index/Utils.js
+++ Framework/Index/Utils.js
@@ -1,17 +1,4 @@
 /*****************************************************************************************
- * exposed to end users, export new utility functions here
+ * utility functions exposed to end users
 *****************************************************************************************/
-
-/**
- * general
- */
-export { delayPromise } from '../Utilities/GeneralUtils';
-export { minMax } from '../Utilities/GeneralUtils';
-export { bubbleSortObjects } from '../Utilities/GeneralUtils';
-export { epochToDDMMYY } from '../Utilities/GeneralUtils';
-export { get_ddmmyyyy_hhmmss } from '../Utilities/GeneralUtils';
-export { ms_to_hhmmss } from '../Utilities/GeneralUtils';
-export { strToUtf8 } from '../Utilities/GeneralUtils';
-export { capitalizeStr } from '../Utilities/GeneralUtils';
-export { objToKeyValueArr } from '../Utilities/GeneralUtils';
-export { getValueByCondition } from '../Utilities/GeneralUtils';
+export * from '../Utilities/GeneralUtils';
