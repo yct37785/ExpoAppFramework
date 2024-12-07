358841bf2d756652c3a4dd4583a04e2f45d726a1
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2024-12-07 15:24:53 +0800

    ✨ new LocalDataManager LocalDataHook merge WIP

--- Framework/Hook/LocalDataHook.js
+++ Framework/Hook/LocalDataHook.js
@@ -1,27 +1,116 @@
-import React, { createContext, useContext, useEffect } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 /**
- * Local data context for LocalDataManager.
+ * Local data context for managing stored data. Returns empty when error state to prevent blocking of app flow.
  */
 export const LocalDataContext = createContext({
-  updateCount: 0,
-  setLocalDataValue: () => {},
-  getLocalDataValue: () => {},
-  resetLocalData: () => {},
-  getLocalDataStringify: () => {}
+  writeLocalData: () => {},
+  readLocalData: () => {},
+  readAllLocalData: () => {},
+  deleteLocalData: () => {},
+  deleteAllLocalData: () => {},
 });
 
 /**
- * Hook for on local data update event.
- * 
- * @param {Function} callback - Triggered when local data update occurs.
+ * Local data manager with built-in update effect.
  */
-export const useLocalDataUpdate = (callback) => {
-  const { updateCount } = useContext(LocalDataContext);
+export const useLocalDataManager = ({ LOCAL_KEY_LIST }) => {
 
-  useEffect(() => {
-    if (updateCount) {
-      callback();
+  /**
+   * Writes a key-value pair to storage.
+   */
+  const writeLocalData = async (key, value) => {
+    try {
+      if (key) {
+        await AsyncStorage.multiSet([[key, JSON.stringify(value)]]);
+      }
+    } catch (e) {
+      console.log(`Failed to write data: ${e.message}`);
     }
-  }, [updateCount, callback]);
+  };
+
+  /**
+   * Reads a value by key from storage.
+   */
+  const readLocalData = async (key) => {
+    try {
+      let kv_list = await AsyncStorage.multiGet([key]);
+      if (kv_list.length === 0) {
+        throw new Error("provided key not found in local storage");
+      } else {
+        return JSON.parse(kv_list.at(0)[1]);
+      }
+    } catch (e) {
+      console.log(`Failed to read data: ${e.message}`);
+      return {};
+    }
+  };
+
+   /**
+   * Retrieves all key values from storage.
+   */
+   const readAllLocalData = async () => {
+    try {
+      const keyList = await AsyncStorage.getAllKeys();
+      let jsonValue = await AsyncStorage.multiGet(keyList);
+      const allData = {};
+      jsonValue.map((keyValPair) => {
+        allData[keyValPair[0]] = JSON.parse(keyValPair[1]);
+      });
+      return allData;
+    } catch (error) {
+      console.log(`Failed to read all data: ${error.message}`);
+      return {}
+    }
+  };
+
+  /**
+   * Deletes key-value pair from storage.
+   */
+  const deleteLocalData = async (key) => {
+    try {
+      await AsyncStorage.multiRemove([key]);
+    } catch (error) {
+      console.log(`Failed to delete data: ${error.message}`);
+    }
+  };
+
+  /**
+   * Deletes all key-value pairs from storage.
+   */
+  const deleteAllLocalData = async () => {
+    try {
+      const keyList = await AsyncStorage.getAllKeys();
+      await AsyncStorage.multiRemove(keyList);
+    } catch (error) {
+      console.log(`Failed to delete all data: ${error.message}`);
+    }
+  };
+
+  return {
+    writeLocalData,
+    readLocalData,
+    readAllLocalData,
+    deleteLocalData,
+    deleteAllLocalData,
+  };
 };
+
+/**
+ * Local data provider for context.
+ */
+export const LocalDataProvider = ({ children, onUpdate }) => {
+  const localDataManager = useLocalDataManager(onUpdate);
+
+  return (
+    <LocalDataContext.Provider value={localDataManager}>
+      {children}
+    </LocalDataContext.Provider>
+  );
+};
+
+/**
+ * Context consumer hook.
+ */
+export const useLocalDataContext = () => useContext(LocalDataContext);
--- Framework/Manager/LocalDataManager.js
+++ Framework/Manager/LocalDataManager.js
@@ -4,92 +4,9 @@ import { objToKeyValueArr } from '../Utility/GeneralUtility';
 const _ = require('lodash');
 
 /**
- * Creates new user data based on LOCAL_DATA_SCHEMA schema, saves it to local storage,
- * and returns a deep copy.
- * 
- * @param {Object} LOCAL_DATA_SCHEMA - The schema for new user data.
- * 
- * @returns {Promise<Object>} A promise that resolves to a deep copy of the new user data.
+ * Custom hook for retrieving and saving KV string pairs to local storage. All KV string pairs in local storage are considered local data.
  */
-async function createNewUserData(LOCAL_DATA_SCHEMA) {
-  try {
-    const keyValList = Object.keys(LOCAL_DATA_SCHEMA).map((key) => {
-      return [key, JSON.stringify(LOCAL_DATA_SCHEMA[key])];
-    });
-    await writeDataAS(keyValList);
-    return _.cloneDeep(LOCAL_DATA_SCHEMA);
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
-
-/**
- * Fixes nested key-value pairs by cloning the value if the key is previously missing.
- * 
- * @param {Object} currObj - The current object to check.
- * @param {Object} templateObj - The template object with default values.
- * 
- * @returns {boolean} True if there were missing keys that were added, false otherwise.
- */
-function fixNestedKeyValues(currObj, templateObj) {
-  let hasMissing = false;
-  for (const key in templateObj) {
-    if (!(key in currObj)) {
-      currObj[key] = _.cloneDeep(templateObj[key]);
-      hasMissing = true;
-    } else if (fixNestedKeyValues(currObj[key], templateObj[key])) {
-      hasMissing = true;
-    }
-  }
-  return hasMissing;
-}
-
-/**
- * Gets all locally saved user data, creates new data if no data is found,
- * and fixes missing nested key-value pairs.
- * 
- * @param {Object} LOCAL_DATA_SCHEMA - The schema for new user data.
- * 
- * @returns {Promise<Object>} A promise that resolves to the user data.
- */
-async function getLocalUserData(LOCAL_DATA_SCHEMA) {
-  try {
-    let allKeys = await getAllKeysAS();
-    
-    // DEBUG ONLY, REMOVE
-    // if (allKeys.length > 0) {
-    //   await deleteDataAS(allKeys);
-    //   allKeys = [];
-    // }
-
-    if (allKeys.length === 0) {
-      return await createNewUserData(LOCAL_DATA_SCHEMA);
-    } else {
-      const userData = await readDataAS(allKeys);
-      const hasMissing = fixNestedKeyValues(userData, LOCAL_DATA_SCHEMA);
-      if (hasMissing) {
-        await writeDataAS(objToKeyValueArr(userData));
-        console.log('saved userData for missing keys');
-      }
-      return userData;
-    }
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
-
-/**
- * Custom hook for managing immutable local data instance and loading/saving to local storage.
- * The data instance can only be modified via functions.
- *
- * @param {Object} LOCAL_DATA_SCHEMA - The schema for new user data.
- */
-const useLocalDataManager = ({ LOCAL_DATA_SCHEMA }) => {
-  const schema = useMemo(() => _.cloneDeep(LOCAL_DATA_SCHEMA), []); // immutable schema
-  const [data, setData] = useState({}); // Local data instance
-  const [isLocalDataLoaded, setIsLocalDataLoaded] = useState(false);
+const useLocalDataManager = ({}) => {
   const [updateCount, setUpdateCount] = useState(0);
 
   /**
