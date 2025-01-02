03ad57ed636caa68f6031a0577dab8434d9c0fee
yct12321 <yct37785@gmail.com>
2025-01-02 23:14:24 +0800

    OptionComp UI TS

--- Framework/UI/Core/Option/OptionComp.tsx
+++ Framework/UI/Core/Option/OptionComp.tsx
@@ -1,34 +1,51 @@
 import React, { useContext, memo, useState } from 'react';
-import { View } from 'react-native';
+import { View, StyleProp, ViewStyle } from 'react-native';
+
+/**
+ * single option props
+ * - each option has a state and holds nested children
+ * 
+ * @param state: 1 = selected, 2 = unselected, 3 = indeterminate
+ */
+export interface IOptionProps {
+  state: number;
+  children?: Record<string, IOptionProps>;
+};
+
+/**
+ * options component props
+ *
+ * @param originalSchema - JSON schema representing the menu options.
+ * @param onSelectionChange - Callback function to handle selection changes: updatedSchema, optionPath, optionRef.
+ * @param optionsContainer - Container to contain children options.
+ * @param renderOption - Function to render the option with the selection control.
+ * @param depthPadding - to apply padding per depth hierarchy prop to same function value.
+ * @param style - Additional style on base container.
+ */
+export interface IOptionCompProps {
+  originalSchema: Record<string, IOptionProps>;
+  onSelectionChange: (updatedSchema: Record<string, IOptionProps>, optionPath: string[]) => void;
+  optionsContainer: React.FC<any>;
+  renderOption: (props: { option: IOptionProps, onPress: () => void }) => JSX.Element;
+  depthPadding?: number;
+  style?: StyleProp<ViewStyle>;
+};
 
 /**
  * General options component for rendering various types of selection controls.
- * 
- * state: 1 = selected, 2 = unselected, 3 = indeterminate
- *
- * @component
- * @param {Object} props - Component props
- * @param {Object} props.originalSchema - JSON schema representing the menu options.
- * @param {Function} props.onSelectionChange - Callback function to handle selection changes: updatedSchema, optionPath, optionRef.
- * @param {Object} props.optionsContainer - Container to contain children options.
- * @param {Function} props.renderOption - Function to render the option with the selection control.
- * @param {number} props.depthPadding - to apply padding per depth hierarchy.
- * prop to same function value.
- * @param {Object} [props.style={}] - Additional style on base container.
- * 
- * @returns {JSX.Element} The OptionsComp component.
  */
-const OptionComp = ({
+const OptionComp: React.FC<IOptionCompProps> = ({
   originalSchema,
   onSelectionChange,
   optionsContainer: OptionsContainer,
   renderOption,
   depthPadding = 0,
-  style={}
+  style = {},
 }) => {
 
-  const deepCloneWithState = (obj, initialState = 1) => {
-    const clone = {};
+  // utility function to clone schema with initial state
+  const deepCloneWithState = (obj: Record<string, IOptionProps>, initialState: number = 1): Record<string, IOptionProps> => {
+    const clone: Record<string, IOptionProps> = {};
     for (const [key, value] of Object.entries(obj)) {
       clone[key] = {
         ...value, // clone existing properties
@@ -41,22 +58,25 @@ const OptionComp = ({
     }
     return clone;
   };
-  const [schema, setSchema] = useState(deepCloneWithState(originalSchema, 2));
+  const [schema, setSchema] = useState<Record<string, IOptionProps>>(deepCloneWithState(originalSchema, 2));
 
-  const handleSelect = (path) => {
+  // handle option selection and state management
+  const handleSelect = (path: string[]) => {
     // always iterate from root as schema refs might change but re-render doesn't activate
     let obj = schema[path[0]];
     let parentsRef = [obj];
-    path.slice(1).map(key => {
-      obj = obj.children[key];
+    path.slice(1).map((key) => {
+      obj = obj.children![key];
       parentsRef.push(obj);
     });
     parentsRef = parentsRef.slice(0, -1).reverse();
+
     // reverse state for node
     obj.state = obj.state === 1 ? 2 : 1;
+
     // check all children state for parent
-    parentsRef.map(parent => {
-      const childrenState = Object.values(parent.children).map(child => child.state);
+    parentsRef.forEach((parent) => {
+      const childrenState = Object.values(parent.children || {}).map((child) => child.state);
       const currChildState = childrenState[0];
       // all children = unselected
       if (currChildState === 2) {
@@ -66,46 +86,47 @@ const OptionComp = ({
       else if (currChildState === 1) {
         parent.state = 1;
       }
-      for (let i = 1; i < childrenState.length; ++i) {
-        // some selected and unselected
-        if (currChildState !== childrenState[i]) {
-          parent.state = 3;
-          break;
-        }
+      else {
+        parent.state = childrenState.every((state) => state === currChildState) ? currChildState : 3;
       }
     });
-    // all children options to match state of current
+
+    // update the states for all children to match the selected state
     if (obj.children) {
-      const setAllToState = (currObj, newState) => {
+      const setAllToState = (currObj: Record<string, IOptionProps>, newState: number) => {
         for (const [key, currChild] of Object.entries(currObj)) {
           currChild.state = newState;
           if (currChild.children) {
             setAllToState(currChild.children, newState);
           }
         }
-      }
+      };
       setAllToState(obj.children, obj.state);
     }
     setSchema({...schema});
     onSelectionChange({...schema}, path);
   };
 
-  const renderChildrenOptions = (options, depth = 0, depthPaddingVal = 0, path = []) => {
+  // recursive rendering of child options
+  const renderChildrenOptions = (options: Record<string, IOptionProps>, depth: number = 0, depthPaddingVal: number = 0, path: string[] = []) => {
     return Object.entries(options).map(([key, option], index) => {
       // track current path/hierarchy
       const optionPath = [...path, key];
+
       // render the parent option/non-leaf node
       if (option.children) {
-        return <View key={index} style={{ paddingLeft: 0 }}>
-          {renderOption({ option, onPress: () => handleSelect(optionPath) })}
-          <View style={{ paddingLeft: depthPaddingVal + depthPadding }}>
-            <OptionsContainer>
-              {renderChildrenOptions(option.children, depth + 1, depthPaddingVal + depthPadding, optionPath)}
-            </OptionsContainer>
+        return (
+          <View key={index} style={{ paddingLeft: 0 }}>
+            {renderOption({ option, onPress: () => handleSelect(optionPath) })}
+            <View style={{ paddingLeft: depthPaddingVal + depthPadding }}>
+              <OptionsContainer>
+                {renderChildrenOptions(option.children, depth + 1, depthPaddingVal + depthPadding, optionPath)}
+              </OptionsContainer>
+            </View>
           </View>
-        </View>
+        );
       } else {
-        // render child option/leaf node
+        // render child option or leaf node
         return <View key={index}>
           {renderOption({ option, onPress: () => handleSelect(optionPath) })}
         </View>
