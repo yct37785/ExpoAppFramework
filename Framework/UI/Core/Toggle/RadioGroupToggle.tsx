4d78c86137bd6d3269d4b74bf37e9502d7234383
yct12321 <yct37785@gmail.com>
2025-01-03 01:24:29 +0800

    RadioGroupToggle UI TS, move style prop from radio group comp to internal view comp

--- Framework/UI/Core/Toggle/RadioGroupToggle.tsx
+++ Framework/UI/Core/Toggle/RadioGroupToggle.tsx
@@ -1,41 +1,62 @@
-import React, { useState, useContext, memo } from 'react';
-import { View } from 'react-native';
+import React, { memo } from 'react';
+import { View, StyleProp, ViewStyle } from 'react-native';
 import { RadioButton } from 'react-native-paper';
 import { Text } from '../Text/Text';
 
 /**
- * Component for rendering one set of radio group
- *
- * @component
- * @param {Object} props - Component props
- * @param {Object} props.options - options in JSON obj: { value: { label: <string> } }
- * @param {string} props.value - value of currently selected option.
- * @param {Function} props.onValueChange - Callback function to handle selection changes.
- * @param {Object} [props.style={}] - Additional style on base container.
+ * radio group options prop
+ * - options are a map where each key has a label property.
  * 
- * @returns {JSX.Element} The RadioGroupToggle component.
- *
+ * @param label - label for each option.
+ */
+interface IRadioGroupOptions {
+  [key: string]: {
+    label: string;
+  };
+};
+
+/**
+ * Props for the RadioGroupToggle component.
+ * 
  * @example
  * const options = {
  *   red: { label: 'Red' },
  *   blue: { label: 'Blue' },
  *   green: { label: 'Green' },
  * };
+ * 
+ * @param options - The options for the radio buttons.
+ * @param value - The value of the currently selected option.
+ * @param onValueChange - Callback function to handle selection changes.
+ * @param style - Additional styles for the container.
  */
-const RadioGroupToggle = ({
+interface IRadioGroupToggleProps {
+  options: IRadioGroupOptions;
+  value: string;
+  onValueChange: (value: string) => void;
+  style?: StyleProp<ViewStyle>;
+};
+
+
+/**
+ * Component for rendering one set of radio group
+ */
+const RadioGroupToggle: React.FC<IRadioGroupToggleProps> = ({
   options,
   value,
   onValueChange,
-  style={}
+  style = {}
 }) => {
   return (
-    <RadioButton.Group onValueChange={newValue => onValueChange(newValue)} style={style} value={value}>
-      <View style={{ flexDirection: 'row' }}>
+    <RadioButton.Group onValueChange={newValue => onValueChange(newValue)} value={value}>
+      <View style={[{ flexDirection: 'row' }, style]}>
         {Object.entries(options).map(([key, obj], index) => {
-          return <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
-            <Text>{obj.label}</Text>
-            <RadioButton value={key} />
-          </View>
+          return (
+            <View key={index} style={{ flexDirection: 'row', alignItems: 'center' }}>
+              <Text>{obj.label}</Text>
+              <RadioButton value={key} />
+            </View>
+          );
         })}
       </View>
     </RadioButton.Group>
