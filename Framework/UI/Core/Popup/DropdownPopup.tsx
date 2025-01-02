ea2f8dbdf178d65c9ffabd715c0304b95a8babde
yct12321 <yct37785@gmail.com>
2025-01-03 00:33:14 +0800

    DropdownPopup UI TS

--- Framework/UI/Core/Popup/DropdownPopup.tsx
+++ Framework/UI/Core/Popup/DropdownPopup.tsx
@@ -1,40 +1,53 @@
-import React, { useRef, memo } from 'react';
+import React, { useRef, memo, ReactNode } from 'react';
 import { TouchableRipple, useTheme } from 'react-native-paper';
-import { Menu, MenuOptions, MenuTrigger } from 'react-native-popup-menu';
+import { Menu, MenuOptions, MenuTrigger, MenuOptionCustomStyle } from 'react-native-popup-menu';
+import { StyleProp, ViewStyle } from 'react-native';
+
+/**
+ * Dropdown Popup Component Props
+ * 
+ * @param triggerComp - The component to trigger the popup. Any valid ReactNode.
+ * @param disabled - Whether the popup trigger is disabled.
+ * @param style - Additional style on the base container.
+ * @param children - The content to display inside the popup.
+ */
+interface IDropdownPopupCompProps {
+  triggerComp: ReactNode;
+  disabled?: boolean;
+  style?: StyleProp<ViewStyle>;
+  children: ReactNode;
+};
 
 /**
  * A general-purpose popup component that can accept any children to open as a dropdown.
  * PLEASE NOTE: triggerComp must not contain onPress callback which overwrites option callbacks
- *
- * @param {Object} props - Component props
- * @param {React.Element} props.triggerComp - The component to trigger the popup.
- * @param {boolean} [props.disabled=false] - Whether the popup trigger is disabled.
- * @param {Object} [props.style={}] - Additional style on base container.
- * @param {React.Element} props.children - The content to display inside the popup.
- * 
- * @returns {JSX.Element} The Popup component.
  */
-function DropdownPopupComp({ 
+const DropdownPopupComp: React.FC<IDropdownPopupCompProps> = ({ 
   triggerComp, 
-  disabled = false,
-  style={},
-  children
-}) {
+  disabled = false, 
+  style = {}, 
+  children 
+}) => {
   const theme = useTheme();
-  const menuRef = useRef(null);
+  const menuRef = useRef<Menu | null>(null);
 
   return (
     <Menu ref={menuRef} style={style}>
-      <MenuTrigger disabled={disabled} customStyles={{
-        TriggerTouchableComponent: TouchableRipple,
-      }}>
+      <MenuTrigger 
+        disabled={disabled} 
+        customStyles={{
+          TriggerTouchableComponent: TouchableRipple,
+        }}
+      >
         {triggerComp}
       </MenuTrigger>
-      <MenuOptions customStyles={{ backgroundColor: theme.colors.surfaceVariant }}>
+      <MenuOptions customStyles={{
+        optionsWrapper: { backgroundColor: theme.colors.surfaceVariant }
+      }}>
         {children}
       </MenuOptions>
     </Menu>
   );
-}
+};
 
 export default memo(DropdownPopupComp);
