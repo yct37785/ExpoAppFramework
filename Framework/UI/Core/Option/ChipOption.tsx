f97a62abf5bb2f904a14208622f2a8e308717b60
yct12321 <yct37785@gmail.com>
2025-01-03 00:02:17 +0800

    ChipOption UI TS

--- Framework/UI/Core/Option/ChipOption.tsx
+++ Framework/UI/Core/Option/ChipOption.tsx
@@ -1,44 +1,41 @@
 import React, { useState, useContext, memo } from 'react';
-import { View, TouchableOpacity } from 'react-native';
+import { View, StyleProp, ViewStyle } from 'react-native';
 import { useTheme, Chip } from 'react-native-paper';
 import { Text } from '../Text/Text';
 import { padSize025, padSize05, padSize, padSize2 } from '../../../Index/Const';
-import OptionComp from './OptionComp';
+import OptionComp, { IOptionProps, onOptionSelectionChangeFunc } from './OptionComp';
 
 /**
- * Component for rendering chip options based on a JSON schema.
- *
- * @component
- * @param {Object} props - Component props
- * @param {Object} props.schema - JSON schema representing the menu options.
- * @param {string} props.schema.label - The label for the menu option.
- * @param {Object} [props.schema.children] - Nested options for the menu.
- * @param {Function} props.onSelectionChange - Callback function to handle selection changes.
- * @param {Object} [props.style={}] - Additional style on base container.
+ * ChipOption component props
  * 
- * @returns {JSX.Element} The ChipOptions component.
- *
- * @example
- * const schema = {
- *   label: 'Colors',
- *   children: {
- *     red: { label: 'Red' },
- *     blue: { label: 'Blue' },
- *     green: { label: 'Green' },
- *   },
- * };
+ * @param schema - JSON schema representing the menu options. Refer to OptionComp @example.
+ * @param schema.label - The label for the menu option.
+ * @param schema.children - Nested options for the menu.
+ * @param onSelectionChange - Callback function to handle selection changes.
+ * @param style - Additional style on base container.
  */
-const ChipOption = ({
+interface IChipOptionCompProps {
+  schema: Record<string, IOptionProps>;
+  onSelectionChange: onOptionSelectionChangeFunc;
+  style?: StyleProp<ViewStyle>;
+};
+
+/**
+ * ChipOption component for rendering chip options based on a JSON schema.
+ */
+const ChipOption: React.FC<IChipOptionCompProps> = ({
   schema,
   onSelectionChange,
-  style={}
+  style = {},
 }) => {
   const theme = useTheme();
 
-  const renderLabel = ({ option, onPress }) => (
+  // render label for parent option
+  const renderLabel = ({ option, onPress }: { option: IOptionProps; onPress: () => void }) => (
     <Text>{option.label}</Text>
   );
 
+  // render chip for each option
   const renderChip = ({ option, onPress }) => (
     <Chip
       selected={option.state === 1}
@@ -59,11 +56,10 @@ const ChipOption = ({
 
   return (
     <OptionComp
-      schema={schema}
+      originalSchema={schema}
       onSelectionChange={onSelectionChange}
       optionsContainer={optionsContainer}
       renderOption={renderChip}
-      renderParentOption={renderLabel}
       style={style} />
   );
 };
