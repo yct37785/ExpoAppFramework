9f923940239ed04fea7baa2d23e9c6bbeccf6d52
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-01-03 17:21:11 +0800

    SampleHomeScreen TS

--- projects/TemplateApp/Screen/SampleHomeScreen.tsx
+++ projects/TemplateApp/Screen/SampleHomeScreen.tsx
@@ -1,29 +1,15 @@
-import React, { useContext, useState, useEffect, useCallback, useRef, memo } from 'react';
+import React, { memo } from 'react';
 import { View } from 'react-native';
 import * as UI from '../../../Framework/Index/UI';
 import * as Hook from '../../../Framework/Index/Hook';
 import * as Const from '../../../Framework/Index/Const';
+import * as PropTypes from '../../../Framework/Index/PropTypes';
 import { ScreenNames } from '../User/ScreenMapper';
-
-const SAMPLE_SCREEN_TITLES: Record<ScreenNames, string> = {
-  [ScreenNames.Home]: "home",
-  [ScreenNames.TextInputBtn]: "text input button example",
-  [ScreenNames.Layouts]: "layouts example",
-  [ScreenNames.Containers]: "containers example",
-  [ScreenNames.Menus]: "menus example",
-  [ScreenNames.Empty]: "empty example",
-  [ScreenNames.DataDisplay]: "data display example",
-};
 
 /**
  * Sample home screen.
- * 
- * @param {Object} props - Component props.
- * @param {Object} props.navigation - React Navigation provided object for navigating between screens.
- * @param {Object} props.route - React Navigation provided object containing route parameters.
  */
-function SampleHomeScreen({ navigation, route }) {
-
+const SampleHomeScreen: React.FC<PropTypes.IScreenProps> = ({ navigation, route }) => {
   const {
     readLocalData,
     writeLocalData
@@ -41,17 +27,24 @@ function SampleHomeScreen({ navigation, route }) {
     </View>
   }
 
+  function renderScreenBtn(screen: ScreenNames, btnText: string) {
+    return <UI.Button onPress={() => navigation.navigate(screen, { paramText: `hello ${screen} from home` })}>
+      {btnText}
+    </UI.Button>
+  }
+
   return (
-    <UI.BasicActivity navigation={navigation} route={route} customHeaderContent={customHeaderContent} title="Home Sample">
+    <UI.BasicActivity navigation={navigation} CustomHeaderComp={customHeaderContent} title="Home Sample">
       <UI.VerticalLayout childMargin={Const.padSize} padding={Const.padSize}>
       <UI.Text variant="bodyMedium">Select the screen you want to navigate to</UI.Text>
-      {Object.keys(SAMPLE_SCREEN_TITLES).map((key) => (
-        <UI.Button key={key} onPress={() => navigation.navigate(key, { paramText: `hello ${key} from home` })}>
-          {SAMPLE_SCREEN_TITLES[key]}
-        </UI.Button>
-      ))}
+        {renderScreenBtn(ScreenNames.TextInputBtn, "text input button example")}
+        {renderScreenBtn(ScreenNames.Layouts, "layouts example")}
+        {renderScreenBtn(ScreenNames.Containers, "containers example")}
+        {renderScreenBtn(ScreenNames.Menus, "menus example")}
+        {renderScreenBtn(ScreenNames.Empty, "empty example")}
+        {renderScreenBtn(ScreenNames.DataDisplay, "data display example")}
       </UI.VerticalLayout>
     </UI.BasicActivity>)
-}
+};
 
 export default memo(SampleHomeScreen);
