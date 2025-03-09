4e075c17137d97c3cc58a2e2333be2f55c7f241a
YC-ACER-LAPTOP\yctan <yiechertan@gmail.com>
2025-03-09 17:48:30 +0800

    Projects folder renamed correctly

---
+++ Projects/TemplateApp/babel.config.js
@@ -0,0 +1,22 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@framework": "@expo-app-framework/framework"
+          }
+        }
+      ]
+    ],
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
+  };
+};
