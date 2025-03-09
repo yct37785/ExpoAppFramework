339fc0c7f56d93cd2f75bc6bb3e878e4bb9b0109
yct12321 <yct37785@gmail.com>
2025-03-09 16:49:28 +0800

    minimal workspace setup with path alias from TemplateApp to framework

--- Framework/UI/Core/Component/Decorator.tsx
+++
@@ -1,7 +0,0 @@
-import React, { memo } from 'react';
-import { Divider, DividerProps } from 'react-native-paper';
-
-/**
- * Divider component.
- */
-export const DividerComp: React.FC<DividerProps> = memo((props) => <Divider {...props} />);
--- projects/TemplateApp/App.tsx
+++ projects/TemplateApp/App.tsx
@@ -1,33 +1,21 @@
-import React from 'react';
-import * as Root from '../../Framework/Index/Root';
-import { LOCAL_DATA_VALUES, DEFAULT_SCREEN, ScreenNames } from './User/Schema';
-import { FC } from 'react';
-import SampleHomeScreen from './Screen/SampleHomeScreen';
-import SampleTextInputBtnScreen from './Screen/SampleTextInputBtnScreen';
-import SampleLayoutsScreen from './Screen/SampleLayoutsScreen';
-import SampleContainersScreen from './Screen/SampleContainersScreen';
-import SampleMenusScreen from './Screen/SampleMenusScreen';
-import SampleEmptyScreen from './Screen/SampleEmptyScreen';
-import SampleDataDisplayScreen from './Screen/SampleDataDisplayScreen';
-import * as UI from '../../Framework/Index/UI';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { HELLO } from '@framework/index';
 
-/**
- * screen map
- */
-export const screenMap: Record<ScreenNames, FC<UI.IScreenProps>> = {
-  [ScreenNames.Home]: SampleHomeScreen,
-  [ScreenNames.TextInputBtn]: SampleTextInputBtnScreen,
-  [ScreenNames.Layouts]: SampleLayoutsScreen,
-  [ScreenNames.Containers]: SampleContainersScreen,
-  [ScreenNames.Menus]: SampleMenusScreen,
-  [ScreenNames.Empty]: SampleEmptyScreen,
-  [ScreenNames.DataDisplay]: SampleDataDisplayScreen,
-}
-
-const App = () => {
+export default function App() {
   return (
-    <Root.RootComp screenMap={screenMap} DEFAULT_SCREEN={DEFAULT_SCREEN} LOCAL_DATA_VALUES={LOCAL_DATA_VALUES} />
+    <View style={styles.container}>
+      <Text>{HELLO}</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
 
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
