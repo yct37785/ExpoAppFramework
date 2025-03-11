0afcc50c09fee15fc92a3e8f20b405cb0333bf78
yct12321 <yct37785@gmail.com>
2025-03-12 03:28:07 +0800

    react-native-dotnet setup and usage with FirebaseConfig

--- Framework/Firebase/FirebaseConfig.ts
+++ Framework/Firebase/FirebaseConfig.ts
@@ -1,16 +1,19 @@
 import { initializeApp, FirebaseApp } from 'firebase/app';
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID
+ } from '@env';
 
 /**
  * firebase
  */
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 }
 
 const firebaseApp: FirebaseApp = initializeApp(firebaseConfig);
--- Projects/TemplateApp/index.ts
+++ Projects/TemplateApp/index.ts
@@ -1,8 +1,5 @@
 import { registerRootComponent } from 'expo';
 import App from './App';
-import dotenv from 'dotenv';
-
-dotenv.config();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
