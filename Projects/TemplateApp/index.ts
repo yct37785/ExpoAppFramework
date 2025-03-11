d47034ad90e7aa3b7971ab37ba25ed86591ae097
yct12321 <yct37785@gmail.com>
2025-03-12 03:08:08 +0800

    FirebaseConfig load from .env

--- Projects/TemplateApp/index.ts
+++ Projects/TemplateApp/index.ts
@@ -1,6 +1,8 @@
 import { registerRootComponent } from 'expo';
-
 import App from './App';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
