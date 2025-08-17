5ad1fcb1901b280d64005e09d3726529f25e0fe3
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2025-08-17 12:45:01 +0800

    start off with minimalist deps, TemplateApp should not have any deps at all

--- Projects/TemplateApp/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RootComp from '@root';
-import { screenMap } from './Screens/ScreenMap';
-
-export default function App() {
-  return (
-    <RootComp DEFAULT_SCREEN='home' screenMap={screenMap} />
-  );
-};
