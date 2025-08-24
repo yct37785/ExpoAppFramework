434544f4aea8ae731ea6a5616a4916df15219507
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2025-08-25 00:37:05 +0800

    reorg ui elems categorization

--- packages/Framework/src/exports/ui.ts
+++ packages/Framework/src/exports/ui.ts
@@ -1,5 +1,6 @@
-export * from '../ui/layout';
-export * from '../ui/options/baseOptions';
-export * from '../ui/options/checkOptions';
+export * from '../ui/menus/options/baseOptions';
+export * from '../ui/menus/options/checkOptions';
+// export * from '../ui/menus/selections/picker';
 export * from '../ui/dialog';
+export * from '../ui/layout';
 export * from '../ui/popup';
--- packages/Framework/src/ui/menus/options/checkOptions.tsx
+++ packages/Framework/src/ui/menus/options/checkOptions.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
 import { View, TouchableOpacity, StyleProp, ViewStyle } from 'react-native';
 import { Text, Checkbox } from 'react-native-paper';
-import * as Const from '../../const';
+import * as Const from '../../../const';
 import { OptionState, OptionSchema, OptionProps, BaseOptions } from './baseOptions';
 
 type CheckOptionCompProps = {
