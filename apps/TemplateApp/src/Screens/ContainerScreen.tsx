47ca2c330c1d8149345e09666c43da12e26f792b
yct37785 <yct37785@gmail.com>
2025-09-05 02:23:11 +0800

    add TemplateApp code back in, declare native modules in package json of TemplateApp

---
+++ apps/TemplateApp/src/Screens/ContainerScreen.tsx
@@ -0,0 +1,101 @@
+import React, { useContext, useState, useEffect, useCallback, useRef, memo } from 'react';
+import { View } from 'react-native';
+import { Divider, Text } from 'react-native-paper';
+import { Core, Const, UI } from 'framework';
+const _ = require('lodash');
+
+/**
+ * Const defines.
+ */
+export const TAB_ROUTES: UI.TabRouteProps[] = [
+  { title: 'Collapsible', key: 'collapsible', icon: 'google-street-view' },
+  { title: 'Page 2', key: 'p2', icon: 'camera' },
+  { title: 'Page 3', key: 'p3', icon: 'palm-tree' },
+];
+
+export const ACCORDION_SECTION_TITLES: string[] = ['First', 'Second', 'Third'];
+
+/******************************************************************************************************************
+ * Containers demo
+ ******************************************************************************************************************/
+const ContainersScreen: React.FC<Core.ScreenProps> = ({ navigation, route }) => {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  /**
+   * renders the scene for each tab
+   */
+  const renderScene: UI.TabsSceneMapFunc = ({ route, jumpTo }) => {
+    switch (route.key) {
+      case 'collapsible':
+        return <CollapsiblePage />;
+      case 'p2':
+        return <Tab2Page />;
+      case 'p3':
+        return <Tab3Page />;
+      default:
+        return null;
+    }
+  }
+
+  /**
+   * collapsible containers demo
+   */
+  const CollapsiblePage = memo(() => {
+    return <UI.VerticalLayout>
+
+      {/* CollapsibleContainer */}
+      <Text variant='titleMedium' style={{ marginTop: Const.padSize }}>CollapsibleContainer</Text>
+      <UI.CollapsibleContainer toggleHeaderText='collapsible'>
+        <View style={{ flex: 1, padding: Const.padSize }}>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+        </View>
+      </UI.CollapsibleContainer>
+
+      <Divider />
+
+      {/* AccordionContainer */}
+      <Text variant='titleMedium'>AccordionContainer</Text>
+      <UI.AccordionContainer sectionTitles={ACCORDION_SECTION_TITLES}>
+        <View style={{ flex: 1, padding: Const.padSize }}>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+        </View>
+        <View style={{ flex: 1, padding: Const.padSize }}>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+        </View>
+        <View style={{ flex: 1, padding: Const.padSize }}>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+        </View>
+      </UI.AccordionContainer>
+
+    </UI.VerticalLayout>;
+  });
+
+  /**
+   * component for page 2
+   */
+  const Tab2Page = memo(() => {
+    return <View style={{ flex: 1 }}><Text>P2</Text></View>;
+  });
+
+  /**
+   * component for page 3
+   */
+  const Tab3Page = memo(() => {
+    return <View style={{ flex: 1 }}><Text>P3</Text></View>;
+  });
+
+  return (
+    <Core.Activity navigation={navigation} title='Container Sample'>
+      {/* tabs container */}
+      <UI.TabsContainer
+        routes={TAB_ROUTES}
+        tabIndex={tabIndex}
+        onTabIdxChange={setTabIndex}
+        position='top'
+        sceneMap={renderScene}
+      />
+    </Core.Activity>
+  );
+};
+
+export default memo(ContainersScreen);
---
+++ apps/TemplateApp/src/Screens/HomeScreen.tsx
@@ -0,0 +1,45 @@
+import React, { useContext, useState, useEffect, useCallback, useRef, memo } from 'react';
+import { View } from 'react-native';
+import { Text, Button, Switch } from 'react-native-paper';
+import { Core, Firebase, Managers, UI } from 'framework';
+
+/******************************************************************************************************************
+ * Home screen
+ ******************************************************************************************************************/
+const HomeScreen: React.FC<Core.ScreenProps> = ({ navigation, route }) => {
+  const { getItem, setItem } = Managers.useLocalData();
+  const isDarkMode = getItem('isDarkMode');
+
+  useEffect(() => {
+    Firebase.getFirebaseApp();
+  }, []);
+
+  function CustomHeader() {
+    return <View style={{ flexDirection: 'row', justifyContent: 'flex-end' }}>
+      <Switch
+        value={!!isDarkMode}
+        onValueChange={(val) => setItem('isDarkMode', val)}
+      />
+    </View>
+  }
+
+  function renderScreenBtn(screen: string, btnText: string) {
+    return <Button mode='contained' onPress={() => navigation.navigate(screen, { paramText: `hello from home` })}>
+      {btnText}
+    </Button>
+  }
+
+  return (
+    <Core.Activity navigation={navigation} CustomHeader={CustomHeader} title='Home Sample' isRootActivity={true}>
+      <UI.VerticalLayout>
+        <Text variant='bodyMedium'>Select the screen you want to navigate to</Text>
+        {renderScreenBtn('layout', 'layouts example')}
+        {renderScreenBtn('menu', 'menus example')}
+        {renderScreenBtn('list', 'list example')}
+        {renderScreenBtn('container', 'containers example')}
+      </UI.VerticalLayout>
+    </Core.Activity>
+  );
+};
+
+export default memo(HomeScreen);
