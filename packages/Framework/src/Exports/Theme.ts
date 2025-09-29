89495e339f683ac7811dce7d9e715f28fb29f179
yct37785 <yct37785@gmail.com>
2025-09-30 00:40:00 +0800

    export themes from Exports dir

---
+++ packages/Framework/src/Exports/Theme.ts
@@ -0,0 +1,4 @@
+export type { Theme } from '../Theme/Theme';
+export { defaultTheme } from '../Theme/default-theme';
+export { myThemeLight } from '../Theme/Custom/my-theme-light';
+export { myThemeDark } from '../Theme/Custom/my-theme-dark';
--- packages/Framework/src/Theme/index.ts
+++
@@ -1,4 +0,0 @@
-export type { Theme } from './Theme';
-export { defaultTheme } from './default-theme';
-export { myThemeLight } from './Custom/my-theme-light';
-export { myThemeDark } from './Custom/my-theme-dark';
