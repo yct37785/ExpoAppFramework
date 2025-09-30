4e4e98238ea77b7ed6a59039ad4fd58e41b0e751
yct37785 <yct37785@gmail.com>
2025-09-30 15:16:49 +0800

    export theme and import from TemplateApp

--- apps/TemplateApp/src/App.tsx
+++ apps/TemplateApp/src/App.tsx
@@ -1,11 +1,11 @@
 import { View } from 'react-native';
 import { screenMap } from './Screens/ScreenMap';
-import { Root } from 'framework';
+import { Root, Theme } from 'framework';
 
 export default function App() {
   return (
     <View style={{ flex: 1 }}>
-      <Root DEFAULT_SCREEN='home' screenMap={screenMap} />
+      <Root DEFAULT_SCREEN='home' screenMap={screenMap} lightTheme={Theme.myThemeLight} darkTheme={Theme.myThemeDark} />
     </View>
   );
 }
