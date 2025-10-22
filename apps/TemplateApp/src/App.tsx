9721280fa816bbef27e2c28331c565e8bc89175d
yct37785 <yct37785@gmail.com>
2025-10-23 00:59:02 +0800

    simplify consumer app root by defining screen map and default layout in App.tsx

--- apps/TemplateApp/src/App.tsx
+++ apps/TemplateApp/src/App.tsx
@@ -1,8 +1,18 @@
 import { View } from 'react-native';
-import { screenMap } from './Screens/ScreenMap';
 import { Root } from 'framework';
 import { Screen, Managers, UI_Core, UI_Derived } from 'framework';
+// screens
+import HomeScreen from './Screens/HomeScreen';
+import TestbedScreen from './Screens/TestbedScreen';
+import TypographyScreen from './Screens/TypographyScreen';
+import LayoutScreen from './Screens/LayoutScreen';
+import MenuScreen from './Screens/MenuScreen';
+import ListScreen from './Screens/ListScreen';
+import ContainerScreen from './Screens/ContainerScreen';
 
+/******************************************************************************************************************
+ * AppBar: default left content
+ ******************************************************************************************************************/
 const DefaultLeftContent = () => (
   <View style={{ flexDirection: 'row', alignItems: 'center', paddingLeft: 12 }}>
     <View
@@ -18,6 +28,22 @@ const DefaultLeftContent = () => (
   </View>
 );
 
+/******************************************************************************************************************
+ * Screen mapper
+ ******************************************************************************************************************/
+export const screenMap: Screen.ScreenMap = {
+  'home': HomeScreen,
+  'testbed': TestbedScreen,
+  'typography': TypographyScreen,
+  'layout': LayoutScreen,
+  'menu': MenuScreen,
+  'list': ListScreen,
+  'container': ContainerScreen,
+};
+
+/******************************************************************************************************************
+ * App with default layout
+ ******************************************************************************************************************/
 const defaultScreenLayoutProps = {
   showTitle: false, // screens can set true to show title
   showBack: undefined, // uses navigation.canGoBack() by default
