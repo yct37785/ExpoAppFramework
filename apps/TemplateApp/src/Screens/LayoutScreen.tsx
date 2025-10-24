045259901d04709027a56fced5c52d3566767ba6
yct37785 <yct37785@gmail.com>
2025-10-24 08:30:22 +0800

    revamp layout screen

--- apps/TemplateApp/src/Screens/LayoutScreen.tsx
+++ apps/TemplateApp/src/Screens/LayoutScreen.tsx
@@ -5,17 +5,20 @@ import { Screen, UI } from 'framework';
 const tier1 = '#ff0000';
 const tier2 = '#2eb82e';
 const tier3 = '#ffff00';
+const small = 50;
+const big = 80;
 
 /******************************************************************************************************************
  * Layouts demo
  ******************************************************************************************************************/
 const LayoutScreen: Screen.ScreenType = ({ navigation, route }) => {
 
-   const BasicContainer: React.FC<{ i: number, backgroundColor?: string }> = ({ i, backgroundColor = tier2 }) => (
-    <View style={{ width: 60, height: 75, alignItems: 'center', justifyContent: 'center', backgroundColor }}>
-      <UI.Text key={i}>child {1 + i}</UI.Text>
-    </View>
-  );
+  const BasicContainer: React.FC<{ i: number, width: number, height: number, backgroundColor?: string }>
+    = ({ i, width, height, backgroundColor = tier2 }) => (
+      <View style={{ width: width, height: height, alignItems: 'center', justifyContent: 'center', backgroundColor }}>
+        <UI.Text>child {i}</UI.Text>
+      </View>
+    );
 
   return (
     <Screen.ScreenLayout>
@@ -23,36 +26,58 @@ const LayoutScreen: Screen.ScreenType = ({ navigation, route }) => {
 
         {/* VerticalLayout: wrap (fills parent height) */}
         <UI.Text variant='labelLarge'>VerticalLayout: wrap</UI.Text>
-        <UI.Text variant='labelMedium'>fills parent height, content wraps within</UI.Text>
+        <UI.Text variant='labelMedium'>constraint: parent height</UI.Text>
         <View style={{ height: 190 }}>
           <UI.VerticalLayout constraint='wrap' backgroundColor={tier1}>
-            {[...Array(7)].map((e, i) => <BasicContainer key={i} i={i} />)}
+            <BasicContainer i={1} width={small} height={small} />
+            <BasicContainer i={2} width={big} height={small} />
+            <BasicContainer i={3} width={big} height={small} />
+            <BasicContainer i={4} width={small} height={big} />
+            <BasicContainer i={5} width={big} height={big} />
+            <BasicContainer i={6} width={big} height={small} />
+            <BasicContainer i={7} width={big} height={small} />
+            <BasicContainer i={8} width={small} height={small} />
           </UI.VerticalLayout>
         </View>
 
         {/* HorizontalLayout: wrap */}
         <UI.Text variant='labelLarge'>HorizontalLayout: wrap</UI.Text>
-        <UI.Text variant='labelMedium'>fills parent width, content wraps within</UI.Text>
+        <UI.Text variant='labelMedium'>constraint: parent width</UI.Text>
         <UI.HorizontalLayout constraint='wrap' backgroundColor={tier1}>
-          {[...Array(7)].map((e, i) => <BasicContainer key={i} i={i} />)}
+            <BasicContainer i={1} width={small} height={small} />
+            <BasicContainer i={2} width={small} height={big} />
+            <BasicContainer i={3} width={small} height={small} />
+            <BasicContainer i={4} width={big} height={big} />
+            <BasicContainer i={5} width={big} height={small} />
+            <BasicContainer i={6} width={small} height={small} />
+            <BasicContainer i={7} width={small} height={big} />
+            <BasicContainer i={8} width={small} height={small} />
         </UI.HorizontalLayout>
 
         {/* HorizontalLayout: wrap */}
         <UI.Text variant='labelLarge'>HorizontalLayout: scroll</UI.Text>
-        <UI.Text variant='labelMedium'>fills parent width, content overflows within</UI.Text>
+        <UI.Text variant='labelMedium'>constraint: none, overflow</UI.Text>
         <UI.HorizontalLayout constraint='scroll' backgroundColor={tier1}>
-          {[...Array(8)].map((e, i) => <BasicContainer key={i} i={i} />)}
+            <BasicContainer i={1} width={small} height={small} />
+            <BasicContainer i={2} width={small} height={small} />
+            <BasicContainer i={3} width={big} height={small} />
+            <BasicContainer i={4} width={small} height={small} />
+            <BasicContainer i={5} width={big} height={small} />
+            <BasicContainer i={6} width={small} height={small} />
+            <BasicContainer i={7} width={big} height={small} />
         </UI.HorizontalLayout>
 
-        {/* layouts nested */}
+        {/* grid */}
         <UI.Text variant='labelLarge'>Layouts nested</UI.Text>
         <UI.HorizontalLayout constraint='none' backgroundColor={tier1}>
+
           <UI.VerticalLayout constraint='wrap' justify='center' flex={1} backgroundColor={tier2}>
-            {[...Array(2)].map((e, i) => <BasicContainer key={i} i={i} backgroundColor={tier3} />)}
+            <BasicContainer i={1} width={small} height={small} backgroundColor={tier3} />
           </UI.VerticalLayout>
           <UI.HorizontalLayout constraint='wrap' justify='center' flex={1} backgroundColor={tier2}>
-            {[...Array(1)].map((e, i) => <BasicContainer key={i} i={i} backgroundColor={tier3} />)}
+            <BasicContainer i={1} width={small} height={small} backgroundColor={tier3} />
           </UI.HorizontalLayout>
+
         </UI.HorizontalLayout>
 
       </UI.VerticalLayout>
