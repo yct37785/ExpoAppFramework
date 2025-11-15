e92b2e21ed092b357e65504352da95277def76ff
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2025-11-16 02:48:09 +0800

    Picker and RadioGroup

--- packages/Framework/src/UI/Core/Selection/RadioGroup.tsx
+++ packages/Framework/src/UI/Core/Selection/RadioGroup.tsx
@@ -1,27 +1,38 @@
 import React, { memo } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { RadioButton, Text } from 'react-native-paper';
 import { RadioGroupType } from './RadioGroup.types';
 
 /******************************************************************************************************************
  * RadioGroup implementation.
  ******************************************************************************************************************/
-export const RadioGroup: RadioGroupType = memo(({
-  options,
-  value,
-  onValueChange,
-  style = {},
-}) => {
-  return (
-    <RadioButton.Group onValueChange={onValueChange} value={value}>
-      <View style={[{ flexDirection: 'row' }, style]}>
-        {Object.entries(options).map(([key, label], index) => (
-          <View key={index} style={{ flexDirection: 'row', alignItems: 'center', marginRight: 12 }}>
-            <Text>{label}</Text>
-            <RadioButton value={key} />
-          </View>
-        ))}
-      </View>
-    </RadioButton.Group>
-  );
+export const RadioGroup: RadioGroupType = memo(
+  ({ options, value, onValueChange, style }) => {
+    return (
+      <RadioButton.Group onValueChange={onValueChange} value={value}>
+        <View style={[styles.row, style]}>
+          {Object.entries(options).map(([key, label]) => (
+            <View key={key} style={styles.item}>
+              <Text>{label}</Text>
+              <RadioButton value={key} />
+            </View>
+          ))}
+        </View>
+      </RadioButton.Group>
+    );
+  }
+);
+
+/******************************************************************************************************************
+ * Styles.
+ ******************************************************************************************************************/
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+  },
+  item: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginRight: 12,
+  },
 });
