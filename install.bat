bd6874c005cfcdf38c6b79664e0581d4dc5a92db
yct37785 <yct37785@gmail.com>
2025-11-23 19:56:41 +0800

    fresh start

--- install.bat
+++ install.bat
@@ -1,15 +1,7 @@
 @echo off
-REM Run cross-platform setup (sync first, then install)
-call npm run setup
-set ERR=%ERRORLEVEL%
+setlocal
 
-echo.
-if %ERR% neq 0 (
-  echo [install] FAILED with exit code %ERR%.
-  echo Press any key to close...
-) else (
-  echo [install] Finished successfully.
-  echo Press any key to close...
-)
-pause >nul
-exit /b %ERR%
+pushd "%~dp0"
+call npm i
+
+endlocal
--- packages/Framework/src/Exports/Screen.ts
+++
@@ -1,3 +0,0 @@
-export * from '../Screen/Screen.types';
-export * from '../Screen/ScreenLayout.types';
-export * from '../Screen/ScreenLayout';
