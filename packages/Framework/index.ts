bd0574f4d8ce9aa1c247b510a366e5621d1146f7
YC-ACER-LAPTOP\yctan <yct37785@gmail.com>
2025-11-29 20:36:19 +0800

    Firebase managers and export

--- packages/Framework/index.ts
+++ packages/Framework/index.ts
@@ -1,5 +1,7 @@
 /**
  * Please update babel.config.js correspondingly.
  */
+export * as Manager from './src/Exports/Manager';
 export { default as Root } from './src/Root/Root';
 export * as Screen from './src/Exports/Screen';
+export * as Util from './src/Exports/Util';
